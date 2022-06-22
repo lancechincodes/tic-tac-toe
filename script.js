d3a41445990bd290b0fd7ffc22a2947c08f70639
lancechincodes <lanceechin@gmail.com>
2022-06-22 16:56:44 -0500

    Complete phase 1 (MVP) - DRYer

--- script.js
+++ script.js
@@ -3,18 +3,9 @@ const MAX_TURNS = 9
 
 // STATE VARIABLES
 let turn = 'X'
-let countSq00 = 0
-let countSq01 = 0
-let countSq02 = 0
-let countSq10 = 0
-let countSq11 = 0
-let countSq12 = 0
-let countSq20 = 0
-let countSq21 = 0
-let countSq22 = 0
-
 
 // CACHED ELEMENT REFERENCES
+const gridContainer = document.querySelector('.grid-container')
 const square00 = document.querySelector('#sq00')
 const square01 = document.querySelector('#sq01')
 const square02 = document.querySelector('#sq02')
@@ -25,9 +16,22 @@ const square20 = document.querySelector('#sq20')
 const square21 = document.querySelector('#sq21')
 const square22 = document.querySelector('#sq22')
 const playerTurn = document.querySelector('#player-turn')
+const resetButton = document.querySelector('#reset')
 
 
 // FUNCTIONS
+const resetGame = function() {
+    square00.innerText = ""
+    square01.innerText = ""
+    square02.innerText = ""
+    square10.innerText = ""
+    square11.innerText = ""
+    square12.innerText = ""
+    square20.innerText = ""
+    square21.innerText = ""
+    square22.innerText = ""
+}
+
 const switchTurn = function() {
     if (turn === 'O') {
         turn = 'X'
@@ -39,130 +43,19 @@ const switchTurn = function() {
     }
 }
 
-const handleSq00 = function() {
-    if (countSq00 === 0) {
+const handleSq = function(e) {
+    console.log(e.target.innerText)
+    if (!e.target.innerText) {
         if (turn === 'O') {
-            square00.innerText = 'O'
+            e.target.innerText = 'O'
         }
         else if (turn === 'X') {
-            square00.innerText = 'X'
+            e.target.innerText = 'X'
         }
         switchTurn()
     }
-    countSq00++
-}
-
-const handleSq01 = function() {
-    if (countSq01 === 0) {
-        if (turn === 'O') {
-            square01.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square01.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq01++
-}
-
-const handleSq02 = function() {
-    if (countSq02 === 0) {
-        if (turn === 'O') {
-            square02.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square02.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq02++
-}
-
-const handleSq10 = function() {
-    if (countSq10 === 0) {
-        if (turn === 'O') {
-            square10.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square10.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq10++
-}
-
-const handleSq11 = function() {
-    if (countSq11 === 0) {
-        if (turn === 'O') {
-            square11.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square11.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq11++
-}
-
-const handleSq12 = function() {
-    if (countSq12 === 0) {
-        if (turn === 'O') {
-            square12.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square12.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq12++
-}
-
-const handleSq20 = function() {
-    if (countSq20 === 0) {
-        if (turn === 'O') {
-            square20.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square20.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq20++
-}
-
-const handleSq21 = function() {
-    if (countSq21 === 0) {
-        if (turn === 'O') {
-            square21.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square21.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq21++
-}
-
-const handleSq22 = function() {
-    if (countSq22 === 0) {
-        if (turn === 'O') {
-            square22.innerText = 'O'
-        }
-        else if (turn === 'X') {
-            square22.innerText = 'X'
-        }
-        switchTurn()
-    }
-    countSq22++
 }
 
 // ATTACH EVENT LISTENERS   
-square00.addEventListener('click', handleSq00)
-square01.addEventListener('click', handleSq01)
-square02.addEventListener('click', handleSq02)
-square10.addEventListener('click', handleSq10)
-square11.addEventListener('click', handleSq11)
-square12.addEventListener('click', handleSq12)
-square20.addEventListener('click', handleSq20)
-square21.addEventListener('click', handleSq21)
-square22.addEventListener('click', handleSq22)
+gridContainer.addEventListener('click', handleSq)
+resetButton.addEventListener('click', resetGame)
