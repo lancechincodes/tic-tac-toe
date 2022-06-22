76af89cd933ea2881051bbd298c9be97ea9e8135
lancechincodes <lanceechin@gmail.com>
2022-06-22 17:15:23 -0500

    Add message if player chooses invalid square

--- script.js
+++ script.js
@@ -6,30 +6,31 @@ let turn = 'X'
 
 // CACHED ELEMENT REFERENCES
 const gridContainer = document.querySelector('.grid-container')
-const square00 = document.querySelector('#sq00')
-const square01 = document.querySelector('#sq01')
-const square02 = document.querySelector('#sq02')
-const square10 = document.querySelector('#sq10')
-const square11 = document.querySelector('#sq11')
-const square12 = document.querySelector('#sq12')
-const square20 = document.querySelector('#sq20')
-const square21 = document.querySelector('#sq21')
-const square22 = document.querySelector('#sq22')
+const sq00 = document.querySelector('#sq00')
+const sq01 = document.querySelector('#sq01')
+const sq02 = document.querySelector('#sq02')
+const sq10 = document.querySelector('#sq10')
+const sq11 = document.querySelector('#sq11')
+const sq12 = document.querySelector('#sq12')
+const sq20 = document.querySelector('#sq20')
+const sq21 = document.querySelector('#sq21')
+const sq22 = document.querySelector('#sq22')
 const playerTurn = document.querySelector('#player-turn')
 const resetButton = document.querySelector('#reset')
+const warningMessage = document.querySelector('#warning-message')
 
 
 // FUNCTIONS
 const resetGame = function() {
-    square00.innerText = ""
-    square01.innerText = ""
-    square02.innerText = ""
-    square10.innerText = ""
-    square11.innerText = ""
-    square12.innerText = ""
-    square20.innerText = ""
-    square21.innerText = ""
-    square22.innerText = ""
+    sq00.innerText = ""
+    sq01.innerText = ""
+    sq02.innerText = ""
+    sq10.innerText = ""
+    sq11.innerText = ""
+    sq12.innerText = ""
+    sq20.innerText = ""
+    sq21.innerText = ""
+    sq22.innerText = ""
 }
 
 const switchTurn = function() {
@@ -45,7 +46,7 @@ const switchTurn = function() {
 
 const handleSq = function(e) {
     console.log(e.target.innerText)
-    if (!e.target.innerText) {
+    if (!e.target.innerText) { // if square does NOT have text
         if (turn === 'O') {
             e.target.innerText = 'O'
         }
@@ -53,6 +54,10 @@ const handleSq = function(e) {
             e.target.innerText = 'X'
         }
         switchTurn()
+    }
+    else { // if square already has text
+        warningMessage.classList.remove('hide')
+        setTimeout(() => warningMessage.classList.add('hide'), 1500)
     }
 }
 
